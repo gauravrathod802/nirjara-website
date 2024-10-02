485ad468b7128c8efdcac1689bfeb07d13696811
gauravrathod802 <98140248+gauravrathod802@users.noreply.github.com>
2024-10-02 22:45:01 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Jewellery-Website/",
+  base: "/nirjara-website/",
   // base: "",
 
   plugins: [react()],
