9dd541c8b48590c74ef8e68e44989c2eaabe6138
gauravrathod802 <98140248+gauravrathod802@users.noreply.github.com>
2024-10-05 20:14:58 +0530

    responsiveness added to tagline

--- src/Component/TagLine/TagLine.jsx
+++ src/Component/TagLine/TagLine.jsx
@@ -3,40 +3,6 @@ import Tagline2 from "../images/Tagline2.png";
 import Tagline3 from "../images/Tagline3.png";
 import './TagLine.css';
 
-// const TagLine = () => {
-//     return (
-//         <div style={bannerStyle}>
-//             <div style={contentContainerStyle}>
-//                 <h1 style={headingStyle}>Pure. Gentle. Ayurvedic.</h1>
-//                 <p style={taglineStyle}>Rejuvenate Your Skin with the Wisdom of Ayurveda.</p>
-//                 <p style={contentStyle}>Welcome to a new age of skincare where ancient Ayurvedic wisdom meets modern convenience. Our <strong>Nirjara Powder Face Wash</strong> is meticulously crafted with the <strong>100% </strong>natural ingredients to bring out your skin's true radiance, all while staying gentle on the skin and kind to nature.</p>
-//                 <img
-//                     src={Tagline1}
-//                     alt="Ayurvedic Face Wash"
-//                     style={productImageStyle}
-//                 />
-//                 <img
-//                     src={Tagline2}
-//                     alt="Ayurvedic Face Wash"
-//                     style={productImageStyle}
-//                 />
-//                 <img
-//                     src={Tagline3}
-//                     alt="Ayurvedic Face Wash"
-//                     style={productImageStyle}
-//                 />
-//             </div>
-//         </div>
-//     )
-// }
-
-
-// import React from 'react';
-// import Tagline1 from "../images/Tagline1.png";
-// import Tagline2 from "../images/Tagline2.png";
-// import Tagline3 from "../images/Tagline3.png";
-// import './TagLine.css';
-
 const TagLine = () => {
     return (
         <div className="banner">
